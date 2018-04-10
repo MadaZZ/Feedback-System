2da98753d036c176721bb390c70749ab76d48474
MadaZZ <sparshranaazz@gmail.com>
2018-04-10 14:34:34 +0530

    added routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { LoginComponent } from './auth/login/login.component';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavComponent } from './navigation/sidenav/sidenav.component';
 import { FeedbackComponent } from './feedback/feedback.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { MaterialModule } from './material/material.module';
 
 
 @NgModule({
@@ -18,7 +20,8 @@ import { FeedbackComponent } from './feedback/feedback.component';
     FeedbackComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
