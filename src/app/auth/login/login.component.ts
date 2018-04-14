446d1fb0504d4464fb88cd1c9d03d1779683475c
MadaZZ <sparshranaazz@gmail.com>
2018-04-14 20:34:02 +0530

    added password field

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -7,7 +7,8 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  hide = true;
+  
   constructor() { }
 
   ngOnInit() {
