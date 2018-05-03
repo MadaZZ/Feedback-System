0d6b198c5fc7106310a9eab5cc27b05f98af1938
MadaZZ <sparshranaazz@gmail.com>
2018-05-03 23:14:02 +0530

    Set student guard

--- src/app/authguards/student.guard.ts
+++ src/app/authguards/student.guard.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class StudentGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+  constructor(private authser: AuthService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.authser.studentg() == true) {
+      return true;
+    }
+    else {
+      this.router.navigate(['/login']);
+    }
   }
 }
