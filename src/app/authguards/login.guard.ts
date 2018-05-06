a838106d4a6cd08c78b4a516484818d66f1966b0
MadaZZ <sparshranaazz@gmail.com>
2018-05-06 13:30:34 +0530

    Designed route guard for /login

--- src/app/authguards/login.guard.ts
+++ src/app/authguards/login.guard.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class LoginGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+  constructor(private authser: AuthService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.authser.studentg() || this.authser.teacherg() || this.authser.adming()) {
+      return false;
+    }
+    else if (this.authser.studentg()) {
+      this.router.navigate(['/student']);
+    }
+    else if (this.authser.teacherg()) {
+      this.router.navigate(['/teacher']);
+    }
+    else if (this.authser.adming()) {
+      this.router.navigate(['/admin']);
+    }
+    else{
+      return true;
+    }
   }
 }
