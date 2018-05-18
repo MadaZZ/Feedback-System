b617ff4cf0568d528d24760bb5efb4f4258a90bc
MadaZZ <sparshranaazz@gmail.com>
2018-05-18 19:16:50 +0530

    Update db

--- dbf/app.js
+++ dbf/app.js
@@ -16,19 +16,48 @@ router.post('/student', function (req, res, next) {
 });
 
 router.post('/addstudent', function (req, res, next) {
-  console.log(req.body);
-  student.create(req.body).then(function (studentdata) {
-    console.log(studentdata);
-    res.send(studentdata);
+  console.log(req.body.id);
+  student.findOne({'id':req.body.id}).then(function (data){
+    if(data == null)
+    {
+      student.create(req.body).then(function (studentdata) {
+        console.log(studentdata+"asdsad");
+        res.send(studentdata);
+      }).catch(next);
+    }
+    else
+    {
+      console.log(data);
+      res.send(null);
+    }
   }).catch(next);
+  
 });
 
 router.post('/addfaculty', function (req, res, next) {
   console.log(req.body);
-  teacher.create(req.body).then(function (studentdata) {
-    console.log(studentdata);
-    res.send(studentdata);
-  }).catch(next);
+  teacher.findOne({'id':req.body.id}).then(function(data1){
+    if(data1 == null)
+    {
+      teacher.findOne({'subject':req.body.subject,'class':req.body.class}).then(function(data2){
+        if(data2 == null)
+        {
+              teacher.create(req.body).then(function(teacherData){
+                res.send(teacherData);
+              }).catch(next);
+        }
+        else
+        {
+          res.send(null);
+        }
+      });
+    }
+    else
+    {
+      res.send(null);
+    }
+  });
+  
 });
 
 router.post('/faculty', function (req, res, next) {
@@ -75,11 +104,17 @@ router.post('/update', function (req, res, next) {
   console.log(req.body);
   let name = req.body.name;
 
-    teacher.update({'name' : name}, {$set: {'rating': req.body.rating}}).then(function (studentdata) {
+    teacher.update({'name' : name}, {$set: {'rating': req.body.rating, 'reviews' : req.body.reviews}}).then(function (studentdata) {
       console.log(studentdata);
       res.send(studentdata);
     }).catch(next);
   });
-  
+  router.get('/searchTeachersName', function (req, res) {
+    let name=req.query.name; 
+    teacher.find({'name' : name}).then(function (data) { 
+      console.log("asdasd " + data);
+      res.send(JSON.stringify(data));
+    });
+  });
 
 module.exports = router;
